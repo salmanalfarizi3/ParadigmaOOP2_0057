5145b72b0064d0a9c073f095b173f8eea822f50a
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-03 14:53:37 +0700

    Menambahkan kelas turunan 'joko' yang mengoverride method pesan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -16,26 +16,4 @@ class joko : public seseorang {
         }
     };
     
-    class lia : public seseorang {
-    public:
-        void pesan() {
-            cout << "Pesan dari lia" << endl;
-        }
-    };
     
-    int main() {
-        seseorang* obyek;
-        joko a;
-        lia b;
-    
-        obyek = &a;
-        obyek->pesan();
-    
-        obyek = &b;
-        obyek->pesan();
-    
-        // a.seseorang::pesan();
-        // b.seseorang::pesan();
-    
-        return 0;
-    }
