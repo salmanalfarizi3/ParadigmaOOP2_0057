4df87ff4106cca82dbc6ed8b77877d6fcfd67a45
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-03 14:54:35 +0700

    Menambahkan kelas turunan 'lia' yang mengoverride method pesan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -16,4 +16,10 @@ class joko : public seseorang {
         }
     };
     
-    
+    class lia : public seseorang {
+        public:
+            void pesan() {
+                cout << "Pesan dari lia" << endl;
+            }
+        };
+        
