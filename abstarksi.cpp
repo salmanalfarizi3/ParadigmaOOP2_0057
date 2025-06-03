9e1b438b3f1244830c8b520efdfead3d2c9e4467
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-03 15:06:29 +0700

    tambahkan method display untuk menampilkan nilai atribut

--- abstarksi.cpp
+++ abstarksi.cpp
@@ -13,4 +13,25 @@ private:
         y = b;
     }
 
+     // menampilkan nilai
+     void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+
+
+class AbstraksiKlas {
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
     
