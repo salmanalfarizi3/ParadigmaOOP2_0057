27cf34e4d207853ad90e779cfbbae63e75df8b56
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-03 15:07:16 +0700

    implementasi dan uji coba AbstraksiKlas di fungsi main

--- abstarksi.cpp
+++ abstarksi.cpp
@@ -34,4 +34,17 @@ public:
         y = b;
     }
 
-    
+    // menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
