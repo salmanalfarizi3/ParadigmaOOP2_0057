1a5be4a6f4b29c8fdc9b886654d23cdfcd7c11ea
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-03 15:04:38 +0700

    ambahkan method setXY untuk mengisi nilai atribut

--- abstarksi.cpp
+++ abstarksi.cpp
@@ -5,3 +5,12 @@ class AbstraksiKlas {
 private:
     string x, y;
 
+    public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    
