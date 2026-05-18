e03ec605a346199eb4fc4e8d12ee26a98303222d
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-18 17:54:45 +0700

    Menampilkan hallo saya function dari derived class

--- NOoverriding.cpp
+++ NOoverriding.cpp
@@ -10,4 +10,14 @@ public:
 
 class derivedClass : public baseClass {
 public:
-    
+    void perkenalan() {
+        cout << "hallo saya function dari derived class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
