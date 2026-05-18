bcc279e390f738e27af7ebf49c525b314d838568
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-18 18:04:52 +0700

    class budi : public pekerja, public pelajar

--- solvemorpi.cpp
+++ solvemorpi.cpp
@@ -32,3 +32,19 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
