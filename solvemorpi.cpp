6a9fc505c529d9f6aaf2daec7a913bafe8c6149c
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-18 18:03:41 +0700

    Menampilkan pesan "orang dibuat denagn umur"

--- solvemorpi.cpp
+++ solvemorpi.cpp
@@ -4,3 +4,11 @@ using namespace std;
 class orang {
 public:
     int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl; 
+    }
+};
+
